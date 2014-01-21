68af490b7c9d5c7de61bbdeb3b50bec3e19642a3
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2014-01-21 12:15:07 -0400

    documenting adaptive_param_plot

--- mpl_plot_templates/adaptive_param_plot.py
+++ mpl_plot_templates/adaptive_param_plot.py
@@ -10,7 +10,9 @@ def multidigitize(x,y,binsx,binsy):
 def linlogspace(xmin,xmax,n):
     return np.logspace(np.log10(xmin),np.log10(xmax),n)
 
-def adaptive_param_plot(x,y,bins=10,threshold=5,
+def adaptive_param_plot(x,y,
+                        bins=10,
+                        threshold=5,
                         marker='.',
                         marker_color=None,
                         ncontours=5,
@@ -19,18 +21,72 @@ def adaptive_param_plot(x,y,bins=10,threshold=5,
                         contourspacing=linlogspace,
                         mesh_alpha=0.5,
                         norm=None,
+                        axis=None,
+                        cmap=None,
                         **kwargs):
     """
     Plot contours where the density of data points to be plotted is too high
+
+    Many of these parameters are just passed on to matplotlib's "plot" and
+    "contour" functions
+
+    Parameters
+    ----------
+    bins: int or ndarray
+        The number of bins or a list of bins.  Passed to np.histogram2d; see
+        their docs for details
+    threshold: int
+        The minimum number of points to replace a bin with a contour.  For
+        npoints<=threshold, the individual points will be plotted
+    marker: str
+        Any valid marker (see matplotlib.plot)
+    marker_color: str
+        Any valid marker color
+    ncontours: int
+        Number of contour levels
+    fill: bool
+        Use filled contours?
+    mesh: bool
+        Use a color mesh instead of contours (i.e., filled pixels)
+    contourspacing: function
+        A function to determine the contour spacing.  Should be 
+        np.linspace or the linlogspace function
+        defined above.  The function must accept arguments of:
+        lowest contour level, highest contour level, number of contours
+    mesh_alpha: float
+        Alpha opacity parameter of the colormesh, if used
+    norm: matplotlib.colors.Normalize
+        A normalization to use for the contours or colormesh
+    axis: None or matplotlib.Axes
+        A matplotlib axis to plot on
+    cmap: matplotlib colormap
+        A valid matplotlib color map
+    kwargs: dict
+        Passed to plot, contour, AND colormesh, so must be valid for ALL 3!
     """
+    
+    if axis is None:
+        axis = pl.gca()
 
     ok = np.isfinite(x) * np.isfinite(y)
 
+    # set the number of bins to be an integer value, which can be extracted
+    # from an array-style bin set
+    if hasattr(bins,'ndim') and bins.ndim == 2:
+        nbinsx,nbinsy = bins.shape
+    else:
+        try:
+            nbinsx = nbinsy = len(bins)
+        except TypeError:
+            nbinsx = nbinsy = bins
+
     H,bx,by = np.histogram2d(x[ok],y[ok],bins=bins)
 
+    # determine the locations of each pixel
     dx,dy = multidigitize(x[ok],y[ok],bx,by)
-    dx[dx==bins+1] = bins
-    dy[dy==bins+1] = bins
+    # anything that belongs past the last bin goes into the last bin instead
+    dx[dx==nbinsx+1] = nbinsx
+    dy[dy==nbinsy+1] = nbinsy
 
     plottable = H <= threshold
     #H[plottable] = np.nan
@@ -43,27 +99,28 @@ def adaptive_param_plot(x,y,bins=10,threshold=5,
     levels = contourspacing(threshold-0.5,H.max(),ncontours)
     #levels = contourspacing(0,H.max(),ncontours)
 
-    cm = mpl.cm.get_cmap()
-    cm.set_under((0,0,0,0))
-    cm.set_bad((0,0,0,0))
+    if cmap is None:
+        cmap = mpl.cm.get_cmap()
+        cmap.set_under((0,0,0,0))
+        cmap.set_bad((0,0,0,0))
 
     if fill:
-        con = pl.contourf(cx,cy,H.T,levels=levels,norm=norm,**kwargs)
+        con = axis.contourf(cx,cy,H.T,levels=levels,norm=norm,cmap=cmap,**kwargs)
     else:
-        con = pl.contour(cx,cy,H.T,levels=levels,norm=norm,**kwargs)
+        con = axis.contour(cx,cy,H.T,levels=levels,norm=norm,cmap=cmap,**kwargs)
     if mesh:
-        mesh = pl.pcolormesh(bx,by,H.T, **kwargs)
+        mesh = axis.pcolormesh(bx,by,H.T, **kwargs)
         mesh.set_alpha(mesh_alpha)
     
     if 'linestyle' in kwargs:
         kwargs.pop('linestyle')
 
-    pl.plot(x[ok][toplot],
-            y[ok][toplot],
-            linestyle='none',
-            marker=marker,
-            markerfacecolor=marker_color,
-            markeredgecolor=marker_color,
-            **kwargs)
+    axis.plot(x[ok][toplot],
+              y[ok][toplot],
+              linestyle='none',
+              marker=marker,
+              markerfacecolor=marker_color,
+              markeredgecolor=marker_color,
+              **kwargs)
 
     return cx,cy,H,x[ok][toplot],y[ok][toplot]
